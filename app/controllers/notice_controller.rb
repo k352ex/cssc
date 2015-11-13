c63d2ae48fbbd31c8dd350cdbbabd66ef890f4bb
k352ex <k352ex@naver.com>
2015-11-14 04:37:49 +0900

    공지 페이지 수정, 게시글 작성 Modal으로 생성

--- app/controllers/notice_controller.rb
+++ app/controllers/notice_controller.rb
@@ -1,5 +1,36 @@
 class NoticeController < ApplicationController
 	def index
+		@post_new = Post.new
+		@post = Post.all
 
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+	    @post = Post.new(post_params)
+
+	    if @post.save
+	      redirect_to root_path
+	    else
+	      flash[:alert] = '실패'
+	      render 'new'
+	   end
+	 end
+
+	def show
+		@post = Post.all
+	end
+
+ 	def update
+
+  	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content, :author)
 	end
 end
