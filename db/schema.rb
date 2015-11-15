524cccc136a9f5de93181ab4a400b307a32f22af
k352ex <k352ex@naver.com>
2015-11-15 16:41:23 +0900

    devise Username 필드 추가, devise 컨트롤러 추가, 메인 페이지 수정, 로그인 & 로그아웃 추가

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151113163651) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
