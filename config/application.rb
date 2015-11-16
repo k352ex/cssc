281617b9e315d8085efac41736b761d850b1b059
k352ex <k352ex@naver.com>
2015-11-16 13:45:19 +0900

    footer 생성 및 Notice index에 pagination 생성, updated_at 시간 년/월/일 로 변경

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,6 @@ module WKUComputer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.time_zone = 'Seoul'
-    config.active_record.default_timezone = 'Seoul'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
